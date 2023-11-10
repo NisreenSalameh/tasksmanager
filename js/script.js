562e42d76a95045534b1bad14d50827b9bd4ab64
nisreen salemeh <nisreensalameh123@gmail.com>
2023-11-10 12:33:34 +0200

    Finished the main functionalities

---
+++ js/script.js
@@ -0,0 +1,152 @@
+const alertMessage = document.querySelector('.alert');
+const noneInputsContainer = document.querySelector('.none-inputs');
+
+const addTask = document.getElementById("addTask");
+const clearAllButton = document.querySelector(".clear-btn");
+
+const dateInput = document.getElementById("taskDueDate");
+const addTaskName = document.getElementById("taskName");
+
+const allTasksButton = document.getElementById("allTasks");
+const activeTasksButton = document.getElementById("activeTasks");
+const completedTasksButton = document.getElementById("completedTasks");
+
+const taskList = document.getElementById("tasksListing");
+const tableRows = taskList.getElementsByTagName("tr");
+
+
+addTask.addEventListener("click", () => {
+    if (validateTaskInput()) {
+        addTaskToTable();
+        noneInputsContainer.style.display = "block";
+    } else {
+        alertMessage.classList.remove("d-none");
+        setTimeout(() => {
+            alertMessage.classList.add("d-none");
+        }, 2500);
+    }
+});
+
+clearAllButton.addEventListener("click", () => {
+    clearAllTasks();
+});
+
+let taskName;
+let dueDate;
+let priority;
+let taskRow;
+
+function validateTaskInput() {
+    taskName = addTaskName.value;
+    dueDate = dateInput.value;
+    priority = document.getElementById("priority").value;
+    return taskName.trim() !== "" && dueDate !== "" && priority !== "";
+}
+
+function addTaskToTable() {
+    taskName = addTaskName.value;
+    dueDate = dateInput.value;
+    priority = document.getElementById("priority").value;
+
+    if (taskName) {
+        taskRow = createTaskRow(taskName, dueDate, priority);
+        taskList.appendChild(taskRow);
+        addTaskName.value = "";
+        dateInput.value = "";
+        document.getElementById("priority").selectedIndex = 0;
+
+
+        const isEmpty = taskList.getElementsByTagName("tr").length <= 1;
+        clearAllButton.classList.toggle("d-none", isEmpty);
+        allTasksButton.click();
+    }
+}
+
+
+function createTaskRow(taskName, dueDate, priority) {
+    taskRow = document.createElement("tr");
+    
+    const statusCell = document.createElement("td");
+    statusCell.textContent = "Active";
+    statusCell.style.fontWeight = "bold";
+    
+    const taskDescription = document.createElement("td");
+    taskDescription.textContent = taskName;
+    
+    const priorityCell = document.createElement("td");
+    priorityCell.textContent = priority;
+
+    if (priority === "High Priority") {
+        priorityCell.style.textDecoration = "underline";
+        priorityCell.style.color = "#ff0000";
+    } else if (priority === "Low Priority") {
+        priorityCell.style.textDecoration = "underline";
+        priorityCell.style.color = "#FFD700";
+    }
+    
+    
+    const dueDateCell = document.createElement("td");
+    const formattedDate = new Date(dueDate).toLocaleDateString("en-US", {
+        month: "numeric",
+        day: "numeric",
+        year: "numeric",
+    });
+    dueDateCell.textContent = formattedDate;
+    
+    const editButton = document.createElement("button");
+    editButton.innerHTML = '<i class="fa-regular fa-pen-to-square"></i>';
+    editButton.addEventListener("click", () => {
+        taskDescription.setAttribute("contenteditable", "true");
+        taskDescription.focus();
+    });
+
+    taskDescription.addEventListener("blur", () => {
+        taskDescription.removeAttribute("contenteditable");
+    });
+
+    const completeButton = document.createElement("button");
+    completeButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    completeButton.addEventListener("click", () => {
+        statusCell.textContent = "Completed";
+        taskDescription.style.textDecoration = "line-through";
+        taskDescription.style.fontWeight = "bold";
+        taskDescription.style.color = "green";
+    });
+    
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    deleteButton.addEventListener("click", () => {
+        taskRow.remove();
+        
+        const remainingTasks = taskList.getElementsByTagName("tr").length - 1;
+
+        if (remainingTasks === 0) {
+            noneInputsContainer.style.display = "none";
+            clearAllButton.classList.add("d-none");
+        }
+    });
+    
+    const actionsCell = document.createElement("td");
+    actionsCell.appendChild(editButton);
+    actionsCell.appendChild(completeButton);
+    actionsCell.appendChild(deleteButton);
+    
+    taskRow.appendChild(statusCell);
+    taskRow.appendChild(taskDescription);
+    taskRow.appendChild(priorityCell);
+    taskRow.appendChild(dueDateCell);
+    taskRow.appendChild(actionsCell);
+    
+    return taskRow;
+}
+
+function clearAllTasks() {
+    const taskList = document.getElementById("tasksListing");
+    const tableRows = taskList.getElementsByTagName("tr");
+
+    for (let i = tableRows.length - 1; i > 0; i--) {
+        tableRows[i].remove();
+    }
+    noneInputsContainer.style.display = "none";
+    clearAllButton.classList.add("d-none");
+}
